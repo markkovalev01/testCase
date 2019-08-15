21bc80d447c01aa1c6749c9091f315efaaa229c8
Mark <kovalevm9609@yandex.ru>
2019-08-15 12:39:51 +0900

    RELEASE 1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> list = StringProcessor.processString("сапог сарай арбуз болт бокс биржа абрикос шашлык помидор хлеб масло кокао человек пончик воля разум сила стая сталь стелька стул стол"," ");
-//        System.out.println(list.getClass().getName());
-//        list.forEach(obj -> System.out.println(obj));
         SortedDictionary sortedDictionary = new SortedDictionary();
         sortedDictionary.setDictionary(list);
-        sortedDictionary.getDictionary().forEach(obj -> System.out.println(obj));
-        System.out.println(sortedDictionary);
+//        sortedDictionary.getDictionary().forEach(obj -> System.out.println(obj));
+//        System.out.println(sortedDictionary);
         System.out.println(sortedDictionary.formattedOut());
     }
 }
